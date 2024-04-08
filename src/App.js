ed43137a90d99066f66c44ec4c1ec892a1c791a1
aaryae <aaryaed@gmail.com>
2024-04-08 16:14:14 +0545

    routing done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React from 'react';
+import React from "react";
 
 import Header from "./components/Header";
-import "./app.css"
+import "./app.css";
 import FeaturedRecipies from "./components/FeaturedRecipies";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
-import RecipeList from './components/RecipeList';
-import RecipeForm from './components/RecipeForm';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import News from './components/News';
-// import Detailrecipe from './components/Detailrecipe';
-
-
+import RecipeList from "./components/RecipeList";
+import RecipeForm from "./components/RecipeForm";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "./components/News";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
- 
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
-      <Banner/>
-      <Routes>
-        <Route path='/' element={<FeaturedRecipies/>} />
-        <Route path='/recipes' element={<RecipeList/>} />
-        <Route path='/addrecipes' element={<RecipeForm/>} />
-        <Route path='/News' element={<News/>} />
-        {/* <Route path='/detailrecipe' element={<Detailrecipe/>} /> */}
-      </Routes>
+        <Header />
+        <Banner />
+        <Routes>
+          <Route path="/" element={<FeaturedRecipies />} />
+          <Route path="/recipes" element={<RecipeList />} />
+          <Route path="/addrecipes" element={<RecipeForm />} />
+          <Route path="/News" element={<News />} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetail />} />{" "}
+        </Routes>
       </BrowserRouter>
-     
-      <Footer/>
-      
-    
+
+      <Footer />
     </div>
   );
 }
--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import data from "../data/recipes.json";
 
-const RecipeDetail = ({ selectedRecipe }) => {
-  const recipiedata = selectedRecipe;
+const RecipeDetail = () => {
+  const { recipeId } = useParams();
+  const recipe = data.find((recipe) => recipe.id.toString() === recipeId);
 
-  if (!recipiedata) {
-    return null;
+  if (!recipe) {
+    return <div>Recipe not found</div>;
   }
 
-
-  const recipeDetails = Object.entries(recipiedata).map(([key, value]) => (
-    <div key={key}>
-      {key}:{value}
-    </div>
-  ));
-
+  const imagePath = require(`../Images/${recipe.image}`);
   return (
-    <div>
-      {recipeDetails}
+    <div className="Recipieshoni max-w-screen-2xl mx-auto mt-20 mb-0 bg-[#ffffff] p-5 ">
+      <img src={imagePath} alt="" />
+      <h2>{recipe.title}</h2>
+      <p>ID: {recipe.id}</p>
+
+      {/* Render other recipe details here */}
     </div>
   );
 };
